453c9825400b07a6bea706b357b61c855e9492d2
Charlie Unfricht <dzcharlie@gmail.com>
2021-07-23 14:07:58 -0400

    Create stubs for PokemonSave ABC and Gen1Save implementation

---
+++ gen1_save.py
@@ -0,0 +1,17 @@
+from pokemon_save import PokemonSave
+
+class Gen1Save(PokemonSave):
+    def get_box_pokemon(self, box_no):
+        return
+
+    def get_casino_coins(self):
+        return
+
+    def get_party_pokemon(self):
+        return
+
+    def get_player_name(self):
+        return
+
+    def get_pokedollars(self):
+        return
---
+++ pokemon_save.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class PokemonSave(ABC):
+    def __init__(self, bytes):
+        self.bytes = bytes
+        super().__init__()
+    
+    @abstractmethod
+    def get_box_pokemon(self, box_no):
+        pass
+
+    @abstractmethod
+    def get_casino_coins(self):
+        pass
+
+    @abstractmethod
+    def get_party_pokemon(self):
+        pass
+
+    @abstractmethod
+    def get_player_name(self):
+        pass
+
+    @abstractmethod
+    def get_pokedollars(self):
+        pass
