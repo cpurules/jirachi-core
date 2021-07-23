6d5bd5985928831cbb112c063a847f0ae8a80553
Charlie Unfricht <dzcharlie@gmail.com>
2021-07-23 14:50:15 -0400

    Add bcd_to_decimal() and fix read_bytes()

--- jirachi_core/utils.py
+++ jirachi_core/utils.py
@@ -2,6 +2,15 @@ class Utils:
     @staticmethod
     def read_bytes(path):
         file_bytes = None
-        with open(path) as f:
+        with open(path, 'rb') as f:
             file_bytes = f.read()
         return file_bytes
+    
+    @staticmethod
+    def bcd_to_decimal(bcd_bytes):
+        dec = 0
+        power = len(bcd_bytes) - 1
+        for b in bcd_bytes:
+            dec = dec + (int(hex(b)[2:]) * pow(10, power * 2))
+            power = power - 1
+        return dec
