75627751a7462ab78200144fa4ce2b74964a7cf8
Charlie Unfricht <dzcharlie@gmail.com>
2021-07-23 15:09:18 -0400

    Add new tests

--- jirachi_core/tests/test_gen1_save.py
+++ jirachi_core/tests/test_gen1_save.py
@@ -11,3 +11,9 @@ def blue_save_1():
 class TestGen1Save:
     def test_get_casino_coins(self, blue_save_1):
         assert blue_save_1.get_casino_coins() == 3455
+    
+    def test_get_pokedollars(self, blue_save_1):
+        assert blue_save_1.get_pokedollars() == 20379
+    
+    def test_get_trainer_id(self, blue_save_1):
+        assert blue_save_1.get_trainer_id() == 14494
