07786d39961563b4a047c64df9a1f5b4d896faf7
Charlie Unfricht <dzcharlie@gmail.com>
2021-07-26 00:14:09 -0400

    Add PokedexEntry

--- jirachi_core/pokedex_entry.py
+++ jirachi_core/pokedex_entry.py
@@ -3,3 +3,12 @@ class PokedexEntry:
         self.id = id
         self.seen = seen
         self.caught = caught
+    
+    def __eq__(self, obj):
+        return (isinstance(obj, PokedexEntry) and
+                self.id == obj.id and
+                self.seen == obj.seen and
+                self.caught == obj.caught)
+    
+    def __str__(self):
+        return "{0},{1},{2}".format(self.id, self.seen, self.caught)
